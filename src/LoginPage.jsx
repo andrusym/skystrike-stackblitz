704c719ef75811f3f7fff5543509c87aeedf2ea9
andrusym <andrusym@gmail.com>
2025-06-07 09:37:57 -0400

    Update LoginPage.jsx

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -15,27 +15,25 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-page">
-        <h2>Login to SkyStrike</h2>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            value={email}
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit">Login</button>
-        </form>
-      </div>
+    <div className="login-page">
+      <h2>Login to SkyStrike</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
